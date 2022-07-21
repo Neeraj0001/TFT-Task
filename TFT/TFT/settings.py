969e6edfebccd50c4ab19cef54fa5d101b1fbe5f
neeraj0001 <np10121999@gmail.com>
2022-07-21 14:11:40 +0530

    postgreSQL TFT db connected

--- TFT/TFT/settings.py
+++ TFT/TFT/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'TFT.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'TFT',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
     }
 }
 
